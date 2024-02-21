dbee22acd84a16ff17d9a964ca6e55107ca16dab
Mehes Nandor <mehesnandor1@gmail.com>
2024-02-21 13:19:47 +0100

    build: add the commons-log3 dependency to pom.xml

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -3,6 +3,8 @@ package nationalize;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class Nationality {
 
@@ -69,5 +71,11 @@ public class Nationality {
         }
 
     }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+    }
+
 
 }
